6f96ab638e988d07445efb7297ba97f75e66c74e
hellobot2001 <79287930+hellobot2001@users.noreply.github.com>
2026-09-16 11:42:25 -0500

    add << and >> operators

--- WeGetThePoint/main.cpp
+++ WeGetThePoint/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 class Point
 {
+	friend istream& operator>>(istream& i, Point& p);
 private:
 	double x;
 	double y;
@@ -15,6 +17,8 @@ public:
 		y = _y;
 	}
 
+	Point() { x = 0; y = 0; }
+
 	double operator-(Point other)
 	{
 		return sqrt(pow(other[0] - x, 2) + pow(other[1] - y, 2));
@@ -25,7 +29,7 @@ public:
 		return (x == other[0]) && (y == other[1]);
 	}
 
-	bool operator==(Point other)
+	bool operator!=(Point other)
 	{
 		return (x != other[0]) || (y != other[1]);
 	}
@@ -63,30 +67,82 @@ public:
 			return y;
 	}
 
-	ostream operator<<(Point p)
-	{
-
-	}
-
 	void print()
 	{
 		cout << "(" << x << ", " << y << ")" << endl;
 	}
 };
 
+ostream& operator<<(ostream& o, Point p)
+{
+	o << "(" << p[0] << ", " << p[1] << ")";
+	return o;
+}
+
+void donothing() {}
+
+istream& operator>>(istream& i, Point& p)
+{
+	string s;
+	i >> s;
+	//cout << s << endl;
+	string num = "";
+	int f = 0;
+	if (s[f] == '(')
+		f++;
+	for (f = f; f < s.length(); f++)
+	{
+		if (s[f] == ',')
+			break;
+		if (s[f] == ' ')
+			break;
+		num.append(string(1, s[f]));
+	}
+	if (s[f] == ',')
+		f++;
+	if (s[f] == ' ')
+		f++;
+	//cout << ":" << num << ":" << endl;
+	p.x = stod(num);
+	num = "";
+	//cout << "hello" << endl;
+	//cout << f << endl;
+	//cout << s.length() << endl;
+	//cout << s << endl;
+	//cout << s[f] << endl;
+	for (f = f; f < s.length(); f++)
+	{
+		//cout << s[f] << endl;
+		if (s[f] == ')')
+			break;
+		num.append(string(1, s[f]));
+	}
+	//cout << num << endl;
+	p.y = stod(num);
+	return i;
+}
+
 int main()
 {
 	Point p(1, 8);
-	p.print();
+	cout << p << endl;
 	++p;
-	p.print();
+	cout << p << endl;
 	p++;
-	p.print();
+	cout << p << endl;
 	--p;
-	p.print();
+	cout << p << endl;
 	p--;
-	p.print();
+	cout << p << endl;
 	cout << p[0] << endl;
 	cout << p[1] << endl;
+
+	cout << "enter coordinates in format x,y or (x,y): ";
+	Point* v2 = new Point();
+	cin >> *v2;
+	cout << *v2 << endl;
+	cout << *v2 - p << endl;
+	cout << *v2 / p << endl;
+	delete v2; //RECONSTRUCT WHAT
 
 }
